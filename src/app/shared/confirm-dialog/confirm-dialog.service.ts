09ccef45255bc57c5fda737f0abbf5c167c853fb
Franco Androetto <androettofranco@gmail.com>
2025-03-22 05:00:06 -0300

    ✨ feat: add dialog for delete hero

---
+++ src/app/shared/confirm-dialog/confirm-dialog.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from './confirm-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmDialogService {
+
+  constructor(private dialog: MatDialog) {}
+
+  openConfirmDialog(message: string): Observable<boolean> {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '400px',
+      data: { message: message }
+    });
+
+    return dialogRef.afterClosed();
+  }
+}
