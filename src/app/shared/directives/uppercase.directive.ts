6a7f03b477e5c1e2be0ca682c326b841c68c1807
Franco Androetto <androettofranco@gmail.com>
2025-03-25 08:35:10 -0300

    ♻️ refac: implement `ControlValueAccessor` for UppercaseDirective

--- src/app/shared/directives/uppercase.directive.ts
+++ src/app/shared/directives/uppercase.directive.ts
@@ -1,14 +1,41 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
-  standalone: true,
   selector: '[appUppercase]',
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => UppercaseDirective),
+      multi: true
+    }
+  ]
 })
 export class UppercaseDirective {
   constructor() {}
 
   @HostListener('input', ['$event']) onInput(event: Event) {
     const input = event.target as HTMLInputElement;
-    input.value = input.value.toUpperCase();
+    const uppercasedValue = input.value.toUpperCase();
+    input.value = uppercasedValue;
+    this.onChange(uppercasedValue); 
   }
+
+  writeValue(value: any): void {
+    if (value) {
+      const input = document.querySelector('[appUppercase]') as HTMLInputElement;
+      if (input) {
+        input.value = value.toUpperCase();
+      }
+    }
+  }
+
+  onChange = (_: any) => {};
+  
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {}
 }
